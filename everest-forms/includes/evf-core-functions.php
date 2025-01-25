f8c1862881608a8cbc09aee73f171199d54ce63f
Justin Gardner <justin@rhynorater.com>
2025-01-25 06:02:03 -0500

    3.0.9

--- everest-forms/includes/evf-core-functions.php
+++ everest-forms/includes/evf-core-functions.php
@@ -1558,13 +1558,14 @@ function evf_get_license_plan() {
 
 	if ( $license_key && is_plugin_active( 'everest-forms-pro/everest-forms-pro.php' ) ) {
 		$license_data = get_transient( 'evf_pro_license_plan' );
-
-		if ( 'error' === $license_data ) {
-			return false;
-		}
-
 		if ( false === $license_data ) {
 			$license_response = EVF_Updater_Key_API::check( array( 'license' => $license_key ) );
+
+			if ( ! $license_response ) {
+				$license_plan = get_option( 'evf_saved_license_plan', 'unknown' );
+				return evf_handle_license_plan_compatibility( $license_plan );
+			}
+
 			$license_data     = json_decode( $license_response );
 
 			if ( ! empty( $license_data->item_name ) ) {
@@ -1575,15 +1576,12 @@ function evf_get_license_plan() {
 					$license_data->item_plan
 				);
 				$license_data->item_plan = trim( $license_data->item_plan );
-
+				update_option( 'evf_saved_license_plan', $license_data->item_plan );
 				set_transient( 'evf_pro_license_plan', $license_data, WEEK_IN_SECONDS );
-			} else {
-				set_transient( 'evf_pro_license_plan', 'error', 6 * HOUR_IN_SECONDS );
-				return false;
 			}
 		}
-
-		return evf_handle_license_plan_compatibility( isset( $license_data->item_plan ) ? $license_data->item_plan : false );
+		$license_plan = isset( $license_data->item_plan ) ? $license_data->item_plan : get_option( 'evf_saved_license_plan', 'unknown' );
+		return evf_handle_license_plan_compatibility( $license_plan );
 	}
 
 	return false;
@@ -1612,7 +1610,7 @@ if ( ! function_exists( 'evf_handle_force_update' ) ) {
  * @param $license_plan License plan.
  */
 function evf_handle_license_plan_compatibility( $license_plan ) {
-	$license_plan = ( 'plus' === $license_plan || 'professional' === $license_plan ) ? 'personal' : $license_plan;
+	$license_plan = ( 'plus' === $license_plan || 'professional' === $license_plan ||'unknown' === $license_plan ) ? 'personal' : $license_plan;
 	return $license_plan;
 }
 
@@ -5346,6 +5344,32 @@ if ( ! function_exists( 'evf_email_send_failed_handler' ) ) {
 			);
 		}
 	}
+}
+
+/**
+ * Get form data by field key.
+ *
+ * @param array  $form_data Form Data.
+ * @param string $key Field Key.
+ *
+ * @return array
+ */
+function evf_get_form_data_by_key( $form_data, $key = null ) {
+
+	$form_data_array = array();
+
+	foreach ( $form_data['form_fields'] as $field_data ) {
+
+		$field_key = isset( $field_data['type'] ) && null !== $field_data['type'] ? $field_data['type'] : '';
+
+		if ( ! empty( $field_key ) ) {
+			if ( $field_key === $key ) {
+				$form_data_array[] = $field_data;
+			}
+		}
+	}
+
+	return $form_data_array;
 }
 
 add_action(
